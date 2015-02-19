622ad7d929a9248ecebdbd647805c16cb4d2ebb2
Ubuntu <ubuntu@ip-172-31-4-87.us-west-2.compute.internal>
2015-02-19 03:15:00 +0000

    Updating add page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res) {
+    res.render('index', {title: 'About'});
+});
+
 module.exports = router;
