f57561703e9c66e52571321a00851cade242a5e5
Thomas Altman <thomasjaltman@gmail.com>
2015-02-21 22:36:39 +0000

    Adding data to the view

--- routes/locations.js
+++ routes/locations.js
@@ -4,7 +4,7 @@ var router  = express.Router();
 
 /* GET Home page */
 router.get('/', function(req, res, next) {
-    res.send('Welcome');
+    res.render('index', { title : "Home" });
 });
 
 module.exports = router;
