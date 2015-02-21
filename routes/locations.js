229dc95ab33e34fba7b61b1817ef8206f6f9b5dc
Thomas Altman <thomasjaltman@gmail.com>
2015-02-21 22:52:14 +0000

    Creating location pages

--- routes/locations.js
+++ routes/locations.js
@@ -5,4 +5,14 @@ var ctrl    = require('../controllers/locations.js');
 /* GET Home page */
 router.get('/', ctrl.home);
 
+/* GET Location  */
+router.get('/location', function(req, res, next) {
+    res.render('index', { title : 'Location'});
+});
+
+/* GET Location Review */
+router.get('/location/review/new', function(req, res, next) {
+    res.render('index', { title: "New Review" });
+});
+
 module.exports = router;
